072f327fd20c61ee939f4793cc30c81a6bb55cf4
Ibrahim <ibrahimdelibasoglu@gmail.com>
2020-12-27 00:32:08 +0300

    back to old version to use my pretrained old model

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+from .models_super import SRResNet
+from .models_super import ConvolutionalBlock
+from .models_super import ResidualBlock
+from .models_super import SubPixelConvolutionalBlock
+from .models_super import Generator
+from .models_super import Discriminator
+
